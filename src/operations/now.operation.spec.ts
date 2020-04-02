e5a313d6cfcc7f52382fd0dc8515859e3250cc13
Dylan Van Bommel <vanbommeldylan@gmail.com>
2020-04-02 19:20:38 +0300

    fix: fix 'now' operation not based on duration

--- src/operations/now.operation.spec.ts
+++ src/operations/now.operation.spec.ts
@@ -1,6 +1,6 @@
 import * as Moment from 'moment';
 import { unitOfTime } from 'moment';
-import { DateRange, extendMoment } from 'moment-range';
+import { extendMoment } from 'moment-range';
 import { nowOperationFactory } from './now.operation';
 
 const moment = extendMoment(Moment);
@@ -8,19 +8,25 @@ const moment = extendMoment(Moment);
 const mockedDate = moment('2018-02-24');
 const nowOperation = nowOperationFactory(mockedDate);
 
-const unitsOfTime: unitOfTime.DurationConstructor[] = ['day', 'week', 'quarter', 'month', 'year'];
+const unitsOfTime: { unit: unitOfTime.DurationConstructor, days: number }[] = [
+  { unit: 'day', days: 1 },
+  { unit: 'week', days: 7 },
+  { unit: 'quarter', days: 90 },
+  { unit: 'month', days: 30 },
+  { unit: 'year', days: 365 }
+];
 
 describe('nowOperation', () => {
 
   describe('getRange($unit, $date)', () => {
 
     unitsOfTime.forEach(unit => {
-      describe(`with '${unit}' as $unit`, () => {
+      describe(`with '${unit.unit}' as $unit`, () => {
 
         it('should work', () => {
-          const range = nowOperation.getRange(unit);
+          const range = nowOperation.getRange(unit.unit);
 
-          expect(range.start).toEqual(mockedDate.clone().subtract(1, unit));
+          expect(range.start).toEqual(mockedDate.clone().subtract(unit.days, 'days'));
           expect(range.end).toEqual(mockedDate.clone());
         });
       });
@@ -30,14 +36,14 @@ describe('nowOperation', () => {
   describe('isOperation($range, $unit)', () => {
 
     unitsOfTime.forEach(unit => {
-      describe(`with '${unit}' as $unit'`, () => {
+      describe(`with '${unit.unit}' as $unit'`, () => {
         it(`should detect`, () => {
           const range = moment.range(
-            mockedDate.clone().subtract(1, unit),
+            mockedDate.clone().subtract(unit.days, 'days'),
             mockedDate.clone()
           );
 
-          expect(nowOperation.isOperation(range, unit)).toBeTruthy();
+          expect(nowOperation.isOperation(range, unit.unit)).toBeTruthy();
         });
       });
     })
--- src/operations/now.operation.ts
+++ src/operations/now.operation.ts
@@ -5,11 +5,41 @@ export { MomentRangeMethods };
 
 const moment = extendMoment(Moment);
 
+const unitAsDays: Record<Moment.unitOfTime.DurationConstructor, number> = {
+  h: 0,
+  hour: 0,
+  hours: 0,
+  m: 0,
+  millisecond: 0,
+  milliseconds: 0,
+  minute: 0,
+  minutes: 0,
+  ms: 0,
+  s: 0,
+  second: 0,
+  seconds: 0,
+  d: 1,
+  day: 1,
+  days: 1,
+  w: 7,
+  week: 7,
+  weeks: 7,
+  M: 30,
+  month: 30,
+  months: 30,
+  Q: 90,
+  quarter: 90,
+  quarters: 90,
+  y: 365,
+  year: 365,
+  years: 365
+};
+
 export const nowOperationFactory = (date: Moment.MomentInput = Date.now()): Operation => ({
   key: 'n',
   getRange: (unit) => {
     return moment.range(
-      moment(date).subtract(1, unit),
+      moment(date).subtract(unitAsDays[unit], 'days'),
       moment(date)
     )
   },
